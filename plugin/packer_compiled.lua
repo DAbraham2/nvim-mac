a8ba492dbb0073b365ada77313496570665d4b1e
Daniel Abraham <daniel.abraham@silabs.com>
2024-11-05 09:30:28 +0100

    added dap

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -134,6 +134,16 @@ _G.packer_plugins = {
     path = "/Users/daabraha/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
+  ["nvim-dap"] = {
+    loaded = true,
+    path = "/Users/daabraha/.local/share/nvim/site/pack/packer/start/nvim-dap",
+    url = "https://github.com/mfussenegger/nvim-dap"
+  },
+  ["nvim-dap-go"] = {
+    loaded = true,
+    path = "/Users/daabraha/.local/share/nvim/site/pack/packer/start/nvim-dap-go",
+    url = "https://github.com/leoluz/nvim-dap-go"
+  },
   ["nvim-lspconfig"] = {
     loaded = true,
     path = "/Users/daabraha/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
