bbe53932def644cebb458c41043b80d8bccc76ed
Daniel Abraham <daniel.abraham@silabs.com>
2025-04-22 16:45:15 +0200

    added vimbegood

--- lua/mlgfroccs/packer.lua
+++ lua/mlgfroccs/packer.lua
@@ -20,6 +20,7 @@ return require('packer').startup(function(use)
             vim.cmd('colorscheme rose-pine')
         end
     })
+    use('ThePrimeagen/vim-be-good')
 
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use("nvim-treesitter/nvim-treesitter-context")
